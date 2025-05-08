eebed51a69e99de001a5ba780e83304b9456d19c
Jason Chang <jasonchang101@gmail.com>
2025-05-07 22:58:59 -0400

    f

--- app/page.tsx
+++ app/page.tsx
@@ -24,11 +24,19 @@ export default function Component() {
     pageStyle: `
       @page {
         size: ${paperSize === "letter" ? "215.9mm 279.4mm" : "210mm 297mm"};
-        margin: 0;
+        margin: 0mm !important;
       }
       @media print {
+        html, body {
+          width: ${paperSize === "letter" ? "215.9mm" : "210mm"} !important;
+          height: ${paperSize === "letter" ? "279.4mm" : "297mm"} !important;
+          margin: 0 !important;
+          padding: 0 !important;
+          box-sizing: border-box !important;
+        }
         body {
-          -webkit-print-color-adjust: exact;
+          -webkit-print-color-adjust: exact !important;
+          print-color-adjust: exact !important;
         }
       }
     `,
@@ -93,15 +101,13 @@ export default function Component() {
               <h1 className="mb-2 text-4xl font-semibold tracking-tight">
                 Jason Chang
               </h1>
-              <p className="max-w-[650px] text-sm font-medium mb-1">
-                I'm a product designer working with founders to
-                design and ship timeless products.
+              <p className="max-w-[450px] text-sm font-medium mb-1">
+                I'm a design engineer. I craft human interfaces that conform necessary function into intuitive form.
               </p>
               <div className="mb-4 text-sm flex items-center gap-2">
                 <a href="https://jchang.cc" className="underline">
                   jchang.cc
                 </a>{" "}
-                (password: day&night)
                 <span className="text-xs font-medium">|</span>
                 <a href="mailto:jason@jchang.cc" className="underline">
                   jason@jchang.cc
@@ -123,7 +129,7 @@ export default function Component() {
             </header>
 
             <div className="mb-8 border-b border-gray-300 dark:border-gray-800" />
-{/* 
+            {/* 
             <div className="space-y-8 mb-8">
               <section>
                 <h2 className="mb-2 text-md font-bold">Education</h2>
@@ -139,42 +145,38 @@ export default function Component() {
               </section>
             </div> */}
 
-            <div className="grid grid-cols-[1fr_1fr] gap-16 mb-8">
+            <div className="grid grid-cols-[1fr_1fr] gap-16 mb-4">
               <section>
                 <div className="mb-8">
-                <h2 className="mb-4 text-base font-semibold">Education</h2>
-                <div>
-                  <h3 className="text-sm font-semibold">
-                    Carnegie Mellon University{" "}
-                    <span className="font-medium text-muted-foreground">
-                      2015 - 2018
-                    </span>
-                  </h3>
-                  <p className="text-sm">B.S. in Information Systems</p>
+                  <h2 className="mb-4 text-base font-semibold">Education</h2>
+                  <div>
+                    <h3 className="text-sm font-semibold">
+                      Carnegie Mellon University{" "}
+                      <span className="font-medium text-muted-foreground">
+                        2015 - 2018
+                      </span>
+                    </h3>
+                    <p className="text-sm">B.S. in Information Systems</p>
+                  </div>
                 </div>
-                </div>
-                <h2 className="mb-4 text-base font-semibold">
-                  Experience
-                </h2>
+                <h2 className="mb-4 text-base font-semibold">Experience</h2>
 
                 <div className="space-y-5">
                   <Experience
-                    title="Product Designer at Gumroad (Antiwork)"
-                    period="2024 – Present | Contract"
+                    title="Design Engineer at Gumroad (Antiwork)"
+                    period="2024 – Present"
                     bullets={[
-                      "Worked with Sahil, the CEO, to ship features for 3 of Antiwork's products, Gumroad, Flexile, and Helper.",
-                      "Built designs in Figma and shipped directly in code to millions of users.",
+                      "Working with Sahil, the CEO, to ship features for 3 of Antiwork's products, Gumroad, Flexile, and Helper.",
+                      "Building designs directly in Cursor and shipped to millions of users.",
                     ]}
                   />
 
                   <Experience
                     title="UI/UX Designer at Procreate"
-                    period="2023 – Present"
+                    period="2023 – 2025"
                     bullets={[
-                      "Lead design for all aspects of user experience for Procreate's digital platforms, used by millions worldwide.",
-                      "Advise regularly on product design and brand architecture.",
-                      //   "Launched new flagship animation app, Procreate Dreams",
-                      //   "Launched new Help Center site and redesigned many aspects of our marketing site"
+                      "Led design for all aspects of user experience for Procreate's digital platforms, used by millions worldwide.",
+                      "Advised regularly on product design and brand architecture.",
                     ]}
                   />
 
@@ -183,7 +185,7 @@ export default function Component() {
                     period="2022 – 2023"
                     bullets={[
                       "Designed digital products with various clients focusing on brand identity and intuitive user experiences.",
-                      "Notable clients include the NFL, Sagent Lending, and the U.S. Department of Education.",
+                      "Notable clients include the NFL and the U.S. Department of Education.",
                     ]}
                   />
 
@@ -198,67 +200,82 @@ export default function Component() {
               </section>
 
               <div className="space-y-8">
-              <div className="space-y-2">
-                  <h2 className="mb-4 text-md font-semibold">
-                    Skills & Tools
-                  </h2>
-                  <p className="text-sm">Figma, Adobe Creative Suite, Blender, HTML/CSS, JavaScript, Python, Cursor</p>
+                <div className="space-y-2">
+                  <h2 className="mb-4 text-md font-semibold">Skills & Tools</h2>
+                  <p className="text-sm">
+                    Cursor, Figma, v0,
+                    <br />
+                    Next, React, Tailwind
+                  </p>
                 </div>
                 <section>
-                    <div className="mb-8">
-                        <h2 className="mb-4 text-base font-semibold">Projects</h2>
+                  <div className="mb-8">
+                    <h2 className="mb-4 text-base font-semibold">Projects</h2>
 
-                        <div className="space-y-5">
-                        <Experience
-                            title="Procreate Marketing Site Redesign"
-                            period="2024"
-                            bullets={[
-                            "Redesigned Procreate's marketing site, notably the homepage, careers page, and product pages, in preparation for Procreate's unprecedented spotlight on the world stage in Apple's iPad Keynote in May 2024.",
-                            ]}
-                            variant="paragraph"
-                        />
+                    <div className="space-y-5">
+                      <Experience
+                        title="Flexile - Payroll & Equity Platform"
+                        period="2025"
+                        bullets={[
+                          "Designed and built features for Flexile, a payroll and equity platform. Shipped payment flows and equity management tools."
+                        ]}
+                        variant="paragraph"
+                      />
 
-                        <Experience
-                            title="Open Source Gumroad's Design System"
-                            period="2024"
-                            bullets={[
-                            "Published Gumroad's design system to the public to support open sourcing Gumroad.",
-                            ]}
-                            variant="paragraph"
-                        />
+                      <Experience
+                        title="Procreate Help Center Redesign"
+                        period="2025"
+                        bullets={[
+                          "Led redesign of Procreate's Help Center with a new design system and migrated forum discussions from Folio to create a cohesive support experience.",
+                        ]}
+                        variant="paragraph"
+                      />
+                      {/* <Experience
+                        title="Procreate Marketing Site Redesign"
+                        period="2024"
+                        bullets={[
+                          "Redesigned Procreate's marketing site, notably the homepage, careers page, and product pages, in preparation for Procreate's unprecedented spotlight on the world stage in Apple's iPad Keynote in May 2024.",
+                        ]}
+                        variant="paragraph"
+                      /> */}
 
-                        <Experience
-                            title="Procreate Dreams Reveal and Launch"
-                            period="2023"
-                            bullets={[
-                            "Designed all digital marketing experiences for the launch of Procreate Dreams, the new flagship animation app from Procreate.",
-                            ]}
-                            variant="paragraph"
-                        />
+                      <Experience
+                        title="Gumroad Marketplace Redesign"
+                        period="2024"
+                        bullets={[
+                          "Designed and shipped a complete redesign of Gumroad's Discover marketplace to increase GMV and improve product discoverability.",
+                        ]}
+                        variant="paragraph"
+                      />
 
-                        <Experience
-                            title="Procreate Help Center Launch"
-                            period="2023"
-                            bullets={[
-                            "Designed and launched a new Help Center for Procreate users to help build a community and find the resources they need.",
-                            ]}
-                            variant="paragraph"
-                        />
+                      <Experience
+                        title="Open Source Gumroad's Design System"
+                        period="2024"
+                        bullets={[
+                          "Published Gumroad's design system to the public to support open sourcing Gumroad.",
+                        ]}
+                        variant="paragraph"
+                      />
 
-                        <Experience
-                            title="NFL+"
-                            period="2022"
-                            bullets={[
-                            "Designed the landing page experience and branding for the NFL's all-new streaming platform, NFL+.",
-                            ]}
-                            variant="paragraph"
-                        />
-                        
-                        </div>
+                      <Experience
+                        title="Procreate Dreams Reveal and Launch"
+                        period="2023"
+                        bullets={[
+                          "Designed all digital marketing experiences for the launch of Procreate Dreams, the new flagship animation app from Procreate.",
+                        ]}
+                        variant="paragraph"
+                      />
+
+                      {/* <Experience
+                        title="NFL+"
+                        period="2022"
+                        bullets={[
+                          "Designed the landing page experience and branding for the NFL's all-new streaming platform, NFL+.",
+                        ]}
+                        variant="paragraph"
+                      /> */}
                     </div>
-                  
-                  
-                  
+                  </div>
                 </section>
               </div>
             </div>
